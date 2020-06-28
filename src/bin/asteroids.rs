90f9d2460a938a0be902484fd9a111df51d5929e
alex <alex@pdp7.net>
2020-06-28 22:20:02 +0200

    Add starfield

--- src/bin/asteroids.rs
+++ src/bin/asteroids.rs
@@ -2,6 +2,11 @@ extern crate sdl;
 
 use std::num::Wrapping;
 
+use modulo::Mod;
+
+use rand::rngs::SmallRng;
+use rand::{Rng, SeedableRng};
+
 use sdl::event::Event;
 
 use retrofw2_rust::controls::*;
@@ -11,12 +16,14 @@ struct Asteroids {
     screen: sdl::video::Surface,
     video_info: sdl::video::VideoInfo,
     height: isize,
+    prop_width: f32,
     pressed_keys: PressedKeys,
     ship: Ship,
     sin_cos_lut: [(f32, f32); 256],
     frames: usize,
     start: std::time::Instant,
     last_frame: std::time::Instant,
+    stars: [Star; 100],
 }
 
 impl Asteroids {
@@ -29,16 +36,20 @@ impl Asteroids {
             screen,
             video_info,
             height,
+            prop_width,
             pressed_keys: PressedKeys::default(),
             ship: Ship {
                 x: prop_width / 2.0,
                 y: 0.5,
                 rot: Wrapping(0u8),
+                dx: 0.0,
+                dy: 0.0,
             },
             sin_cos_lut: retrofw2_rust::geom::get_sin_cos_lut(),
             frames: 0,
             start: std::time::Instant::now(),
             last_frame: std::time::Instant::now(),
+            stars: make_stars(prop_width),
         }
     }
 
@@ -68,6 +79,22 @@ impl Asteroids {
                 self.ship.rot -= std::num::Wrapping(4);
             }
 
+            if self.pressed_keys.is_pressed(CONTROL_B) {
+                let d = self.sin_cos_lut[self.ship.rot.0 as usize];
+                self.ship.dx += d.0;
+                self.ship.dy += d.1;
+            }
+
+            self.ship.dx *= 0.9;
+            self.ship.dy *= 0.9;
+
+            for mut star in self.stars.iter_mut() {
+                star.x -= self.ship.dx * (star.depth as f32) / 500_000.0;
+                star.y -= self.ship.dy * (star.depth as f32) / 500_000.0;
+                star.x = star.x.modulo(self.prop_width);
+                star.y = star.y.modulo(1.0);
+            }
+
             self.screen.fill(sdl::video::Color::RGB(0, 0, 0));
             self.frames += 1;
 
@@ -77,6 +104,7 @@ impl Asteroids {
                     pixels: std::boxed::Box::new(pixels),
                 };
                 self.draw_ship(&mut painter);
+                self.draw_stars(&mut painter);
                 true
             };
             self.screen.with_lock(_draw);
@@ -90,7 +118,10 @@ impl Asteroids {
         }
 
         sdl::quit();
-        println!("{}", 1000.0 * self.frames as f32 / self.start.elapsed().as_millis() as f32);
+        println!(
+            "{}",
+            1000.0 * self.frames as f32 / self.start.elapsed().as_millis() as f32
+        );
     }
 
     fn draw_ship(&self, painter: &mut Painter) {
@@ -132,6 +163,18 @@ impl Asteroids {
         );
     }
 
+    fn draw_stars(&self, painter: &mut Painter) {
+        for star in self.stars.iter() {
+            painter.put_pixel(
+                (star.x * self.height as f32) as isize,
+                (star.y * self.height as f32) as isize,
+                star.depth,
+                star.depth,
+                star.depth,
+            );
+        }
+    }
+
     #[allow(clippy::too_many_arguments)]
     fn draw_polar_line(
         &self,
@@ -161,6 +204,26 @@ struct Ship {
     x: f32,
     y: f32,
     rot: Wrapping<u8>,
+    dx: f32,
+    dy: f32,
+}
+
+#[derive(Default, Copy, Clone)]
+struct Star {
+    x: f32,
+    y: f32,
+    depth: u8,
+}
+
+fn make_stars(prop_width: f32) -> [Star; 100] {
+    let mut stars = [Star::default(); 100];
+    let mut rng = SmallRng::from_entropy();
+    for mut star in stars.iter_mut() {
+        star.x = rng.gen_range(0.0, prop_width);
+        star.y = rng.gen_range(0.0, 1.0);
+        star.depth = rng.gen_range(0, 255);
+    }
+    stars
 }
 
 fn main() {
